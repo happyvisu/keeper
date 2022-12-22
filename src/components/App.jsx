0988517e3bf6e4ab9b656629f78c946409f56777
happyvisu <happyvisu@yahoo.com>
2022-12-22 20:30:07 +0530

    Updating

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React ,{useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
-function App() {
+
+function App() { 
+
+  const [notes, setNotes]=useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote]
+    }) ;
+  }
+
+  function deleteNote(id) { 
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map(x => (<Note 
-        key={x.key}
-        title={x.title}
-        content={x.content}
-      /> ))}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note 
+        key={index} 
+        id={index} 
+        title={noteItem.title} 
+        content={noteItem.content} 
+        onDelete={deleteNote} />
+        );
+      })}
+      
       <Footer />
     </div>
   );
